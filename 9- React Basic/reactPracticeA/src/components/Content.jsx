1ce830879482794b47db9abf417db5baa5d68a74
Javier Figueroa <109968115+javiernfigueroa@users.noreply.github.com>
2023-05-02 11:54:16 -0400

    update Practice

---
+++ 9- React Basic/reactPracticeA/src/components/Content.jsx
@@ -0,0 +1,14 @@
+export function Content() {
+  return (
+    <div className="content">
+      <h1 className="content-h1">Fun facts about React</h1>
+      <ul>
+        <li>Was first realsd ins 2013</li>
+        <li> Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on GitHub</li>
+        <li>Is mainteined by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </div>
+  );
+}
---
+++ 9- React Basic/reactPracticeA/src/components/Header.jsx
@@ -0,0 +1,15 @@
+export function Header() {
+  return (
+    <header>
+      <nav>
+        <img
+          className="nav-logo"
+          src="src/assets/images/react-logo.png"
+          alt=""
+        />
+        <h3>ReactFacts</h3>
+        <h4>React Course - Proyect 1</h4>
+      </nav>
+    </header>
+  );
+}
